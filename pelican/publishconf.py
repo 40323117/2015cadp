5a1dcb728af9467492ca9ba131e25f3b0f0ccafa
40323117 <40323117@gm.nfu.edu.tw>
2016-01-14 15:39:40 +0800

    40323117-1

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),(' my github', 'https://github.com/40323105/cadpa'),(' my Vimeo', 'https://vimeo.com/home/myvideos'),(' my class ', 'http://2015fallhw.github.io/2015fallcadpa/user/40323105/'),(' my group', 'http://2015fallhw.github.io/2015fallcadpa/category/g2.html'))
+         ('Python', 'http://python.org/'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
